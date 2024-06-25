8907fd66688c31e8481eb7c95dc0eef1f37f2b5e
Bruno Membrado <brunomembrado10@gmail.com>
2024-06-25 11:56:57 -0300

    card component

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,27 +1,12 @@
+import Card from "@/components/Card";
 import { sampleData } from "@/lib/data";
 import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="w-full grid place-items-center gap-4 md:grid-cols-2 xl:grid-cols-3">
       {sampleData.products.map((product) => (
-        <div key={product.id} className="card bg-base-100 w-96 shadow-xl">
-          <figure className="w-96 h-40 overflow-hidden">
-            <img
-              src={product.image}
-              alt="Shoes"
-              className="object-cover w-full h-full"
-            />
-          </figure>
-
-          <div className="card-body">
-            <h2 className="card-title">{product.name}</h2>
-            <p>{product.description}</p>
-            <div className="card-actions mt-4">
-              <p className=" text-xl">${product.price}</p>
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
-          </div>
-        </div>
+        <Card key={product.id} product={product} />
       ))}
     </div>
   );
