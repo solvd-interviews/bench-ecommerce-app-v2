c31aa1cba16459cca0ac8da68c63d47867695afa
Peter <pedromoyano454@gmail.com>
2024-07-29 20:52:01 -0300

    Restrict Card description and fix height

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -7,7 +7,7 @@ const Card = ({ product }: { product: Product }) => {
   return (
     <div className="card bg-base-100 sm:w-96 w-full md:w-80 lg:w-96 shadow-xl rounded-none rounded-t-xl overflow-hidden">
       <Link href={`/${product.id}`}>
-        <figure className="w-full relative">
+        <figure className="w-full relative h-480">
           <Image
             src={product.images[0]}
             alt={product.name}
@@ -20,7 +20,7 @@ const Card = ({ product }: { product: Product }) => {
       </Link>
       <div className="card-body">
         <h2 className="card-title truncate">{product.name}</h2>
-        <p className="overflow-hidden text-ellipsis">{product.description}</p>
+        <p className="overflow-hidden text-ellipsis h-20">{product.description}</p>
         <div className="card-actions mt-4">
           <p className="text-xl">${product.price}</p>
           <BuyButton product={product} />
